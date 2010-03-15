fc8a44117d44011a36d1bea909b280d1685040c7
Scott Patten <spatten@scott-pattens-macbook-pro.local>
2010-03-15 12:58:25 -0700

    first shot at a notes.js that works with both Gears and HTML5 DB storage

--- javascripts/notes.js
+++ javascripts/notes.js
@@ -1,12 +1,12 @@
 $(function() {
-  $('#noteSubmitter').click(function(event) {submitNote(); event.preventDefault();}); //clicking on the submit button creates a note
-  $('#noteCreateForm').submit(function(event) {submitNote(); event.preventDefault();}); //pressing enter on the text input creates a note
-  if($.html5.supportsOffline()) {
-    setupDatabase();
-    getNotes();    
-  } else {
-    $('body').append("Oh noes, I do not support offline DB storage!")
-  }
+  var adaptor = $.html5.supportsOffline() ? 'webkit' : 'gears'
+  note = new Note(adaptor)
+
+  note.getNotes();    
+
+  $('#noteSubmitter').click(function(event) {note.submitNote(); event.preventDefault();}); //clicking on the submit button creates a note
+  $('#noteCreateForm').submit(function(event) {note.submitNote(); event.preventDefault();}); //pressing enter on the text input creates a note
+
   
   if($.html5.supportsLocalStorage()) {
     //do something
@@ -15,47 +15,55 @@ $(function() {
   }
 });
 
-function setupDatabase() {
-  notesDB = openDatabase("notes", "", "The Example Notes App!", 1048576);
-  notesDB.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Notes(title TEXT, body TEXT)', [], nullDataHandler, errorHandler);
-  })
+var Note = function(adaptor) {
+  this.init(adaptor)
+}
+
+Note.onNoteSelect = function(note, notes_list) {
+  logger("adding to notes list: " + note.title)
+  notes_list.append("<li>"+ note.title + ": " + note.text +"</li>"    )
+}
+
+Note.onNoteSave = function(note, notes_list) {
+  logger("the new note has a title of " + note.title + " and a key of " + note.key)
+  Note.onNoteSelect(note, notes_list)
+}
+
+Note.prototype = {
   
-}
+  init:function(adaptor) {
+    this.adaptor = adaptor
+    this.notes_list = $('#notesList > ul')
+    this.setupDatabase()
+  },
+  
+  setupDatabase:function() {
+    logger("getting DB")
+    this.notesDB = new Lawnchair({table: 'notes', adaptor: this.adaptor})
+    this.versionDB = new Lawnchair({table: 'version', adaptor: this.adaptor})
+  },
 
-function nullDataHandler(tx, results) {
-  //no-op
-}
-
-function errorHandler(tx, results) {
-  alert("Error!")
-}
-
-function submitNote() {
-  notesDB.transaction(function(tx) {
+  submitNote:function() {
     var title = $('#titleText').val();
-    var text = $('#bodyText').val();  
-    logger("writing " + title + "," + text + " to Notes")    
-    tx.executeSql('INSERT INTO Notes VALUES(?, ?)', [title, text], getNotes);
-  })
-}
+    var text = $('#bodyText').val();
+    logger("saving note " + title) 
+    that = this
+    this.notesDB.save({title: title, text: text}, function(note) {Note.onNoteSave(note, that.notes_list)})
+  },
 
-function getNotes() {
-  notesDB.transaction(function(tx) {
-    tx.executeSql('SELECT * from NOTES', [], notesSelectHandler, errorHandler)
-  })
-}
+  getNotes:function(notes_list) {
+    logger("getting notes")
+    that = this
+    this.notesDB.each(function(note) {Note.onNoteSelect(note, that.notes_list)})
+  },
 
-function notesSelectHandler(tx, results) {
-  var html = "<ul>"
-  for(var i=0; i < results.rows.length; i++) {
-    var row = results.rows.item(i);
-    html += "<li>"+ row['title']+ ": " + row['body'] +"</li>"    
-  }
-  html += "</ul>"
-  $('#notesList').html(html)
 }
 
 function logger(text) {
-  $("#logger").html(text)
+  if (!!window.console && !!window.console.log) {
+    console.log(text)
+  } else {
+    $("#logger").append(text)
+  }
+  
 }
